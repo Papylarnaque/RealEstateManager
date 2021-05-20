f2a5f51debcca52bc77e49b77da003c2fbc07f1c
Papylarnaque <descamps.etienne@gmail.com>
2021-05-20 19:46:55 +0200

    refactor fileutils

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/FileUtils.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/FileUtils.kt
@@ -1,6 +1,10 @@
 package com.openclassrooms.realestatemanager.utils
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContract
 import java.io.File
 import java.io.InputStream
 import java.io.OutputStream
@@ -26,4 +30,27 @@ fun generateFilename(source: Source) = "$source-${System.currentTimeMillis()}.jp
 
 fun copyImageFromStream(input: InputStream, outputStream: OutputStream) {
         input.copyTo(outputStream)
+}
+
+class MimeTypesUtil : ActivityResultContract<Array<String>, Uri?>() {
+    override fun createIntent(
+        context: Context,
+        input: Array<String>
+    ): Intent {
+        return Intent(Intent.ACTION_GET_CONTENT)
+            .addCategory(Intent.CATEGORY_OPENABLE)
+            .setType("*/*")
+            .putExtra(Intent.EXTRA_MIME_TYPES, input);
+    }
+
+    override fun getSynchronousResult(
+        context: Context,
+        input: Array<String>
+    ): SynchronousResult<Uri?>? {
+        return null
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        return if (intent == null || resultCode != Activity.RESULT_OK) null else intent.data
+    }
 }
